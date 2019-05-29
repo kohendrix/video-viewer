fe48f1ed7d8da008eb9934f786789fc2eb7ddf16
kohei <andokohei@gmail.com>
2019-05-29 17:50:31 +0900

    added SearchBar component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 export default class App extends React.Component {
   render() {
     return (
       <>
-        <div>Hello App</div>
+        <div>
+          <SearchBar />
+        </div>
       </>
     );
   }
