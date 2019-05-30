59c63022ccd458f3f8c6a17d6cfff126a3532922
kohei <andokohei@gmail.com>
2019-05-30 17:15:29 +0900

    added styled and semantic ui css modules

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,20 @@
+import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
 import SearchBar from './SearchBar';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 1100px;
+  margin: 20px auto;
+`;
 
 export default class App extends React.Component {
   render() {
     return (
       <>
-        <div>
+        <Container>
           <SearchBar />
-        </div>
+        </Container>
       </>
     );
   }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Segment, Form, Input, Label, FormField, Container } from 'semantic-ui-react';
+
+// const Container = styled.div`
+//   margin: 20px;
+// `;
 
 export default class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(this.state.term);
+  };
+
   render() {
     return (
       <>
-        <div>SearchBar</div>
+        <Container>
+          <Segment>
+            <Form onSubmit={this.onSubmit}>
+              <FormField>
+                <label>Video Search</label>
+                <Input
+                  name="search"
+                  placeholder="title..."
+                  type="text"
+                  value={this.state.term}
+                  onChange={e => this.setState({ term: e.target.value })}
+                  action={{ icon: 'search', type: 'submit' }}
+                />
+              </FormField>
+            </Form>
+          </Segment>
+        </Container>
       </>
     );
   }
