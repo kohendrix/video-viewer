11466af387cbe72a0ec1d6e1aaacf0268585eb5d
kohei <andokohei@gmail.com>
2019-05-31 17:44:35 +0900

    added youtube video object interface

---
+++ src/interfaces/video.ts
@@ -0,0 +1,33 @@
+export interface Video {
+  etag: string;
+  id: {
+    kind: string;
+    videoId: string;
+  };
+  kind: string;
+  snippet: {
+    channelId: string;
+    channelTitle: string;
+    description: string;
+    liveBroadcastContent: string;
+    publishedAt: string;
+    thumbnails: {
+      default: {
+        url: string;
+        width: number;
+        height: number;
+      };
+      high: {
+        url: string;
+        width: number;
+        height: number;
+      };
+      medium: {
+        url: string;
+        width: number;
+        height: number;
+      };
+    };
+    title: string;
+  };
+}
