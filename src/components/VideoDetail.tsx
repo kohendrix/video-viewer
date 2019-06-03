26d07b958d4ed266c84ae12f7f9f357c5d71c31f
kohei <andokohei@gmail.com>
2019-06-03 11:42:36 +0900

    removed un used imports

--- src/components/VideoDetail.tsx
+++ src/components/VideoDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Video } from '../interfaces/video';
 import { Segment, Header, Embed } from 'semantic-ui-react';
